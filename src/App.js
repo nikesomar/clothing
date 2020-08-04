d39c9b4c3cf05b5bd5c251ca17546759131bd984
omarnikhil <nikhil.omar@cognizant.com>
2020-08-05 00:37:26 +0530

    till sign in redirect to homepage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
-
+import {Switch, Route, Redirect} from 'react-router-dom';
+import {connect} from 'react-redux';
 import './App.css';
 import Homepage from './pages/homepage/homepage.compnent';
 import Header from './components/header/header.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
+import {setCurrentUser} from './redux/user/user.actions';
 
 
-function App() {
-  return (
+class App extends React.Component {
+
+   unsubscribefromAuth = null;
+
+   componentDidMount(){ 
+
+    const {setCurrentUser} = this.props;
+
+     this.unsubscribefromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+        const userRef =await createUserProfileDocument(userAuth);
+        
+        userRef.onSnapshot(snapShot =>{
+          setCurrentUser({
+              id: snapShot.id,
+              ...snapShot.data()
+            
+          });
+        });
+      }
+      else{
+        setCurrentUser( userAuth);
+      }
+     });
+   }
+
+   componentWillUnmount(){
+     this.unsubscribefromAuth();
+   }
+
+   render() {
+   return (
     <div>
-      <Header />
+      <Header  />
       <Switch>
       <Route exact path='/' component={Homepage} />
       <Route path='/shop' component={ShopPage} />
-      <Route path='/signin' component={SignInAndSignUpPage} />
+      <Route 
+        exact 
+        path='/signin' 
+        render={() =>
+          this.props.currentUser ? (
+             <Redirect to='/' /> 
+          ) : (
+            <SignInAndSignUpPage />
+          )
+        }  
+        />
       </Switch>
     </div>
   );
 }
+}
+const mapStateToProps = ({user}) => ({
+  currentUser : user.currentUser
+});
 
-export default App;
+const mapDispatchToProps = dispatch =>({
+  setCurrentUser:  user => dispatch(setCurrentUser(user))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -14,6 +14,32 @@ const config=
     measurementId: "G-93RT0WVFQ8"
   }
 
+  export const createUserProfileDocument= async(userAuth, additionalData)=> {
+    if (!userAuth) return;
+
+    const userRef= firestore.doc(`users/${userAuth.Uid}`);
+
+    const snapShot= await userRef.get();
+
+    if(!snapShot.exists){
+      const{displayName, email} = userAuth;
+      const craetedAt =new Date();
+
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          craetedAt,
+          ...additionalData
+        })
+      }catch (error){
+          console.log('error creating user', error.message);
+      }
+    }
+    return userRef; 
+  }
+
+
   firebase.initializeApp(config);
 
   export const auth =firebase.auth();
